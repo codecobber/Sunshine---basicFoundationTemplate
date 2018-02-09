7080640474f4ee77821f50a7b3a972c8d21bc7ec
Cobber <codecobber@gmail.com>
2018-02-09 11:20:10 +0000

    Add files via upload

---
+++ agents/index.php
@@ -0,0 +1,157 @@
+<?php include('includes/header.inc.php'); ?><div id='start'>
+
+
+       <div id="bck_1" class="bck"><div id="block_1" class="row columns rowNo01">
+<h4 style="margin: 0;" class="text-center">BREAKING NEWS</h4>
+
+</div></div><div id="bck_2" class="bck"><div id="block_2" class="row rowNo19">        <div class="large-9 large-centered columns">
+            <p class="text-center">Lorem ipsum dolor sit amet, nulla cras penatibus sodales nunc vestibulum ut, maecenas suscipit, mi sem dolor leo wisi id, leo vitae eget posuere purus aliquam morbi. Vulputate fusce sapien non, tristique in non amet ac vitae tristique, aliquam vivamus a, ipsum malesuada sagittis nulla, id praesent morbi rhoncus morbi luctus. Ligula odio enim sed nisl diam urna.
+          </p>
+       </div>
+</div></div><div id="bck_3" class="bck"><div id="block_3" class="row small-up-3 medium-up-4 large-up-5 rowNo04">
+<div class="column">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+<div class="column">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+<div class="column">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+<div class="column show-for-medium">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+<div class="column show-for-large">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+</div></div><div id="bck_4" class="bck"><div id="block_4" class="row small-up-3 medium-up-4 large-up-5 rowNo04">
+<div class="column">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+<div class="column">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+<div class="column">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+<div class="column show-for-medium">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+<div class="column show-for-large">
+<img class="imgPlace" src="http://placehold.it/400x370&text=Look at me!" alt="image for article">
+</div>
+</div></div><div id="bck_5" class="bck"><div id="block_5" class="row rowNo14">
+  <div class="medium-6 large-3 columns">
+    <h3>Lorum</h3>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  <div class="medium-6 large-3 columns">
+    <h3>Ipsum</h3>
+    <p>
+      Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.
+    </p>
+  </div>
+  <div class="medium-6 large-3 columns">
+    <h3>Dolor</h3>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  <div class="medium-6 large-3 columns">
+    <h3>Sit Amet</h3>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+</div></div><div id="bck_6" class="bck"><div id="block_6" class="row rowNo35">
+  <div class="medium-6 large-3 columns">
+   <img class="imgPlace" src="http://placehold.it/150x150&text=Look at me!" alt="image for article">
+
+    <h3>Lorum</h3>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.
+    </p>
+  </div>
+  <div class="medium-6 large-3 columns">
+  <img class="imgPlace" src="http://placehold.it/150x150&text=Look at me!" alt="image for article">
+    <h3>Ipsum</h3>
+    <p>
+      Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.
+    </p>
+  </div>
+  <div class="medium-6 large-3 columns">
+  <img class="imgPlace" src="http://placehold.it/150x150&text=Look at me!" alt="image for article">
+    <h3>Dolor</h3>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  <div class="medium-6 large-3 columns">
+  <img class="imgPlace" src="http://placehold.it/150x150&text=Look at me!" alt="image for article">
+    <h3>Sit Amet</h3>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+</div></div><div id="bck_7" class="bck"><div id="block_7" class="row rowNo35">
+  <div class="medium-6 large-3 columns">
+   <img class="imgPlace" src="http://placehold.it/150x150&text=Look at me!" alt="image for article">
+
+    <h3>Lorum</h3>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.
+    </p>
+  </div>
+  <div class="medium-6 large-3 columns">
+  <img class="imgPlace" src="http://placehold.it/150x150&text=Look at me!" alt="image for article">
+    <h3>Ipsum</h3>
+    <p>
+      Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.
+    </p>
+  </div>
+  <div class="medium-6 large-3 columns">
+  <img class="imgPlace" src="http://placehold.it/150x150&text=Look at me!" alt="image for article">
+    <h3>Dolor</h3>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  <div class="medium-6 large-3 columns">
+  <img class="imgPlace" src="http://placehold.it/150x150&text=Look at me!" alt="image for article">
+    <h3>Sit Amet</h3>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+</div></div><div id="bck_8" class="bck"><div id="block_8" class="row column text-center rowNo37">  <hr>
+</div></div><div id="bck_9" class="bck"><div id="block_9" class="row small-up-1 medium-up-2 large-up-3 rowNo39"><div class="column">
+    <div>
+        <p><img class="imgPlace" src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B">
+        </p>
+        <p class="lead">Copernican Revolution caused an uproar</p>
+    </div>
+</div>
+<div class="column">
+    <div>
+        <p><img class="imgPlace" src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B">
+        </p>
+        <p class="lead">Copernican Revolution caused an uproar</p>
+    </div>
+</div>
+<div class="column">
+    <div>
+        <p><img class="imgPlace" src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B">
+        </p>
+        <p class="lead">Copernican Revolution caused an uproar</p>
+    </div>
+</div></div></div><div id="bck_10" class="bck"><div id="block_10" class="row small-up-1 medium-up-2 large-up-3 rowNo39"><div class="column">
+    <div>
+        <p><img class="imgPlace" src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B">
+        </p>
+        <p class="lead">Copernican Revolution caused an uproar</p>
+    </div>
+</div>
+<div class="column">
+    <div>
+        <p><img class="imgPlace" src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B">
+        </p>
+        <p class="lead">Copernican Revolution caused an uproar</p>
+    </div>
+</div>
+<div class="column">
+    <div>
+        <p><img class="imgPlace" src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B">
+        </p>
+        <p class="lead">Copernican Revolution caused an uproar</p>
+    </div>
+</div></div></div><div id="bck_11" class="bck"><div id="block_11" class="row rowNo19">        <div class="large-9 large-centered columns">
+            <p class="text-center">Lorem ipsum dolor sit amet, nulla cras penatibus sodales nunc vestibulum ut, maecenas suscipit, mi sem dolor leo wisi id, leo vitae eget posuere purus aliquam morbi. Vulputate fusce sapien non, tristique in non amet ac vitae tristique, aliquam vivamus a, ipsum malesuada sagittis nulla, id praesent morbi rhoncus morbi luctus. Ligula odio enim sed nisl diam urna.
+          </p>
+       </div>
+</div></div></div><?php include('includes/footer.inc.php'); ?>
